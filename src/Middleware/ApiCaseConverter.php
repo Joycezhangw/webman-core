add57c16ed5021adc713af016af439b0f44c7752
Joyce <787027175@qq.com>
2025-02-01 16:16:23 +0800

    feat(middleware): 兼容webman，优化验证器返回信息

--- src/Middleware/ApiCaseConverter.php
+++ src/Middleware/ApiCaseConverter.php
@@ -3,7 +3,6 @@
 namespace Landao\WebmanCore\Middleware;
 
 use Landao\WebmanCore\Helpers\CamelHelper;
-use Symfony\Component\HttpFoundation\ParameterBag;
 use Webman\Http\Request;
 use Webman\Http\Response;
 use Webman\MiddlewareInterface;
@@ -13,46 +12,31 @@ class ApiCaseConverter implements MiddlewareInterface
     public function process(Request $request, callable $handler): Response
     {
         $this->convertRequestNameCase($request);
-        $response = $handler($request);
-        $this->convertResponseNameCase($response);
-        return $response;
+        return $handler($request);
     }
 
     /**
      * 请求数据驼峰转下划线
-     * @param $request
+     * @param Request $request
      */
-    private function convertRequestNameCase($request): void
+    private function convertRequestNameCase(Request $request): void
     {
-        $this->convertParameterNameCase($request->request);;
-        $this->convertParameterNameCase($request->query);;
-        $this->convertParameterNameCase($request->files);;
-        $this->convertParameterNameCase($request->cookies);;
+        // 获取所有参数
+        $postParams = $request->post();
+        $queryParams = $request->get();
+//        $fileParams = $request->file();
+//        $cookieParams = $request->cookie();
+
+        // 转换参数名称
+        $postParams = CamelHelper::recursiveConvertParameterNameCase($postParams);
+        $queryParams = CamelHelper::recursiveConvertParameterNameCase($queryParams);
+//        $fileParams = CamelHelper::recursiveConvertParameterNameCase($fileParams);
+//        $cookieParams = CamelHelper::recursiveConvertParameterNameCase($cookieParams);
+
+        // 更新请求参数
+        $request->setPost($postParams);
+        $request->setGet($queryParams);
+//        $request->setFile($fileParams);
+//        $request->setCookie($cookieParams);
     }
-
-    /**
-     * 将驼峰命名转下划线命名
-     * @param ParameterBag $parameterBag
-     */
-    private function convertParameterNameCase($parameterBag): void
-    {
-        $parameters = $parameterBag->all();
-        $parameterBag->replace(CamelHelper::recursiveConvertParameterNameCase($parameters));
-    }
-
-
-    /**
-     * 响应数据下划线转驼峰
-     * @param $response
-     */
-    private function convertResponseNameCase($response): void
-    {
-        $content = $response->getContent();
-        $json = json_decode($content, true);
-        if (is_array($json)) {
-            $json = CamelHelper::recursiveConvertNameCaseToCamel($json);
-            $response->setContent(json_encode($json));
-        }
-    }
-
 }
