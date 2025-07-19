4093e19361eabf4033a63e64cd4f88c32610097f
Joyce <787027175@qq.com>
2025-07-19 22:34:47 +0800

    feat(middleware):配置响应的中间件

--- src/config/plugin/landao/webman-core/middleware.php
+++ src/config/plugin/landao/webman-core/middleware.php
@@ -1,10 +1,20 @@
 <?php
-use Landao\WebmanCore\Validation\ValidatorMiddleware;
-use Landao\WebmanCore\Middleware\ApiCaseConverter;
 
+use Landao\WebmanCore\Middleware\RequestCaseConverter;
+use Landao\WebmanCore\Validation\ValidatorMiddleware;
+use Landao\WebmanCore\Middleware\DesensitizeMiddleware;
+use Landao\WebmanCore\Middleware\ResponseCaseConverter;
+
+/**
+ * 中间是洋葱模型
+ * 1、请求时候执行顺序：从上到下
+ * 2、响应时候执行顺序：从下到上
+ */
 return [
     '@' => [
-        ApiCaseConverter::class,
-        ValidatorMiddleware::class,
+        RequestCaseConverter::class,//将请求参数转为驼峰命名
+        ValidatorMiddleware::class,//注解验证
+        ResponseCaseConverter::class,//将响应参数转为下划线命名
+        DesensitizeMiddleware::class//注解脱敏
     ]
 ];
